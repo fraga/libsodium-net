59a169d6cd3b88305cf858570ea37afca9ba3f65
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-07-06 22:20:40 -0400

    Implement crypto_pwhash_scryptxsalsa208sha256 x64

--- libsodium-net/PasswordHash.cs
+++ libsodium-net/PasswordHash.cs
@@ -7,6 +7,8 @@ namespace Sodium
   {
     public const uint SCRYPT_SALSA208_SHA256_BYTES = 102U;
 
+    [DllImport(SodiumCore.LIBRARY_X64, EntryPoint = "crypto_pwhash_scryptxsalsa208sha256", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int _SCRYPTX_SALSA208_SHA256_X64(byte[] buffer, long bufferLen, byte[] password, long passwordLen, byte[] salt, long opsLimit, int memLimit);
   }
 }
 
