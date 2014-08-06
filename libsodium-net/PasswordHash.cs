8fb18605fd7f314c6768eb2fe112d3cb703e7714
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-08-05 21:00:48 -0400

    implement HashSalsa208Sha256

--- libsodium-net/PasswordHash.cs
+++ libsodium-net/PasswordHash.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Sodium
 {
   public class PasswordHash
   {
     public const uint SCRYPT_SALSA208_SHA256_BYTES = 102U;
+
+    public static byte[] HashSalsa208Sha256(byte[] password, byte[] salt, long opsLimit, int memLimit)
+    {
+      if (password == null || salt == null)
+        throw new ArgumentNullException("Password and salt cannot be null");
+
+      if (opsLimit <= 0 || memLimit <= 0)
+        throw new ArgumentException("opsLimit or memLimit cannot be zero or negative");
+
+      byte[] buffer = new byte[password.LongLength];
+
+      int ret = 0;
+
+      if (SodiumCore.Is64)
+        ret = _SCRYPTX_SALSA208_SHA256_X64(buffer, buffer.LongLength, password, password.LongLength, salt, opsLimit, memLimit);
+      else
+        ret = _SCRYPTX_SALSA208_SHA256_X86(buffer, buffer.LongLength, password, password.LongLength, salt, opsLimit, memLimit);
+
+      if (ret != 0)
+        throw new Exception(String.Format("Internal error, check for proper libsodium version - {0}", SodiumCore.SodiumVersionString));
+
+      return buffer;
+    }
 
     [DllImport(SodiumCore.LIBRARY_X64, EntryPoint = "crypto_pwhash_scryptxsalsa208sha256", CallingConvention = CallingConvention.Cdecl)]
     private static extern int _SCRYPTX_SALSA208_SHA256_X64(byte[] buffer, long bufferLen, byte[] password, long passwordLen, byte[] salt, long opsLimit, int memLimit);
