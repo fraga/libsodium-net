ee75e8b17a96a871a54c1bf00765deeb0edebebf
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2016-01-06 00:02:31 -0500

    we don't need osx deps script

--- travis-osx-deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-
-mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./libsodium-net.sln
-echo "mono --runtime=v4.0  ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe \"\$@\"" > nunit-console.sh
